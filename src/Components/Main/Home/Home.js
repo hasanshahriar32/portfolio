6d2fbca8cb57dce067cc5457448099040bc310e7
Mr. Hasan <hasanshahriar32@gmail.com>
2022-12-12 18:49:05 +0600

    updated home

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Home = () => {
-  return <div>this is home</div>;
+  return (
+    <div>
+      <iframe
+        src="https://my.spline.design/glasstypecopy-62071413d2e799033c06d142458b9e36/"
+        frameborder="0"
+        width="100%"
+        height="500px"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Home;
