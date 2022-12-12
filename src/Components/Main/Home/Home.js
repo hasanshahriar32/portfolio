38b7fd5e2cc9924220a33c9d1f77ad9843611e90
Mr. Hasan <hasanshahriar32@gmail.com>
2022-12-13 03:09:58 +0600

    i frame updated

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      // eslint-disable-next-line jsx-a11y/iframe-has-title
       <iframe
+        title="Spline Design"
         src="https://my.spline.design/glasstypecopy-62071413d2e799033c06d142458b9e36/"
         frameborder="0"
         width="100%"
